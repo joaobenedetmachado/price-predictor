f497e4a2b79386aac8ede6bf6cf511df61d37ade
joao machado <jvbm2177@gmail.com>
2025-10-02 19:03:23 -0300

    primeira rota

--- api/app.py
+++ api/app.py
@@ -1,3 +1,12 @@
 import tensorflow as tf
 import csv
 import fastapi
+import pandas as pd 
+
+app = fastapi.FastAPI()
+
+@app.post("/dataset")
+def create_dataset(file_path: str):
+    data = pd.read_csv(file_path)
+    dataset = tf.data.Dataset.from_tensor_slices(dict(data))
+    return dataset
