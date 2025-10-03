5334612119ac6f44343e9adff8ef8e1326d096ab
joao machado <jvbm2177@gmail.com>
2025-10-03 17:47:47 -0300

    added if to /predict api to avoid non data/price format

--- api/app.py
+++ api/app.py
@@ -8,6 +8,9 @@ app = FastAPI()
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     df = pd.read_csv(file.file)
+    if "Price" not in df.columns or "Date" not in df.columns:
+        return {"error": "CSV must contain 'Price' and 'Date' columns."}
     print(df.head())
     return {"actual": df["Price"].tolist(), "predicted": df["Price"].tolist()}
+    
     
