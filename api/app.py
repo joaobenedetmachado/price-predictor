9407b9c1e889b5757feacc975f6f239709e42b7d
joao machado <jvbm2177@gmail.com>
2025-10-04 09:45:11 -0300

    starting split def

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import tensorflow_decision_forests as tfdf
+import numpy as np
 import csv
 from fastapi import FastAPI, File, UploadFile
 import pandas as pd 
@@ -7,6 +9,43 @@ from datetime import date, timedelta
 app = FastAPI()
 
 df = pd.DataFrame()
+
+import tensorflow_decision_forests as tfdf
+import pandas as pd
+
+import pandas as pd
+import tensorflow_decision_forests as tfdf
+
+def split_train_test(df):
+    df["Date"] = pd.to_datetime(df["Date"])
+
+    df["Year"] = df["Date"].dt.year
+    df["Month"] = df["Date"].dt.month
+    df["Day"] = df["Date"].dt.day
+
+    df = df.drop(columns=["Date"])
+
+    dataset = tfdf.keras.pd_dataframe_to_tf_dataset(df, label="Price")
+
+    for features, label in dataset.take(5):
+        print("Features:", features)
+        print("Label:", label.numpy())
+
+
+
+def return_pridictions(data, range_values):
+    data = data.drop_duplicates()
+    data["Date"] = pd.to_datetime(data["Date"])
+    last_date = data["Date"].max()
+    future_dates = [last_date + timedelta(days=i) for i in range(1, range_values + 1)] # increment 1 by 1 to show the future dates
+    
+    split_train_test(data)
+    # value_predicted = predict_by_range(test, train, range)
+    # ruturn value_predicted    
+    
+    return future_dates
+
+
 
 @app.post("/predict")
 async def predict(range_values: int, file: UploadFile = File(...)):
@@ -21,10 +60,3 @@ async def predict(range_values: int, file: UploadFile = File(...)):
         return_formated_values.append(str(i)[:10])  
 
     return return_formated_values
-
-def return_pridictions(data, range_values):
-    data = data.drop_duplicates()
-    data["Date"] = pd.to_datetime(data["Date"])
-    last_date = data["Date"].max()
-    future_dates = [last_date + timedelta(days=i) for i in range(1, range_values + 1)] # increment 1 by 1 to show the future dates
-    return future_dates
