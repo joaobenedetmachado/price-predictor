7220f4418d7796c45803d06256d4a544a95a66c6
joao machado <jvbm2177@gmail.com>
2025-10-04 09:20:03 -0300

    formating output dates

--- api/app.py
+++ api/app.py
@@ -15,12 +15,16 @@ async def predict(range_values: int, file: UploadFile = File(...)):
         return {"error": "CSV must contain 'Price' and 'Date' columns."}
 
     return_values = return_pridictions(df, range_values)
-    return "oi"
+
+    return_formated_values = []
+    for i in return_values:
+        return_formated_values.append(str(i)[:10])  
+
+    return return_formated_values
 
 def return_pridictions(data, range_values):
     data = data.drop_duplicates()
     data["Date"] = pd.to_datetime(data["Date"])
     last_date = data["Date"].max()
     future_dates = [last_date + timedelta(days=i) for i in range(1, range_values + 1)] # increment 1 by 1 to show the future dates
-    print(future_dates)
     return future_dates
