bdf10a6f0cf21cfa1856a057bd1d1ca80075a923
joao machado <jvbm2177@gmail.com>
2025-10-04 10:26:00 -0300

    frontend

--- api/app.py
+++ api/app.py
@@ -6,8 +6,17 @@ import csv
 from fastapi import FastAPI, File, UploadFile
 import pandas as pd 
 from datetime import date, timedelta
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 df = pd.DataFrame()
 
